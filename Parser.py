dd2065d0acfa95e977027abc6c3c44576dac3e03
D H <hanspal.singhd@gmail.com>
2018-06-03 21:14:59 -0400

    Initiale concept for a parser. I still need to add writing to csv funcitionality and the ability to go to the next page to collect the data

---
+++ Parser.py
@@ -0,0 +1,11 @@
+from bs4 import BeautifulSoup as bs
+import requests
+
+page = requests.get('https://basketball.realgm.com/nba/stats')
+page_data = bs(page.content, 'html.parser')
+table = page_data.find('table', {'class': 'tablesaw compact'})
+# table_data = table.find('tbody')
+table_rows = table.find_all('tr')
+for row in table_rows:
+  row_list = list(map(lambda a: a.get_text(), row.find_all('td')))
+  print(','.join(row_list[1:]))
