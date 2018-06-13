1c8f56cbe9f4c3af4b882b9a8411d99ef7239020
hanspal.singhd@gmail.com <H@ns945369.com>
2018-06-12 23:06:56 -0400

    Alpha .01 features finalized, parser updated

--- Parser.py
+++ Parser.py
@@ -1,14 +1,20 @@
 from bs4 import BeautifulSoup as bs
 import requests
+from postgres.connect import connect
+
+conn = connect('player-stats', 'nba-dev', 'nba-dev')
+cur = conn.cursor()
 
 # page = requests.get('https://basketball.realgm.com/nba/stats')
 page = requests.get('http://www.nbaminer.com/nbaminer_nbaminer/basic_stats_pl.php?operation=eexcel&partitionpage=&partition2page=&page=1')
 page_data = bs(page.content, 'html.parser')
-table = page_data.find('table', {'class': 'tablesaw compact'})
-# table_data = table.find('tbody')
 table = page_data.find('table')
 table_rows = table.find_all('tr')
-for row in table_rows:
+# Table stores headers as a table row
 table_headers = table_rows[0]
+# Iterate index 1..rows
 for row in table_rows[1:]:
   row_list = list(map(lambda a: a.get_text(), row.find_all('td')))
+  # Col 1 is First + Last name
+  # Needs to be split before inserting into the db 
+  print(row_list)
